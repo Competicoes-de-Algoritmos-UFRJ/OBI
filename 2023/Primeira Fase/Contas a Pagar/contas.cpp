8cd0d500a9e0f8bcfab458631a68db3130c3932b
Tiago Silva <tiago.pds.silva@gmail.com>
2023-06-09 18:42:41 -0300

    Create contas.cpp

---
+++ 2023/Primeira Fase/Contas a Pagar/contas.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int valor, contas[3];
+
+    cin >> valor;
+
+    for(int i=0; i<3; i++){
+        cin >> contas[i];
+    }
+
+    sort(contas, contas+3);
+
+    int resp=0;
+
+    for(int i=0; i<3; i++){
+        if(valor>=contas[i]){
+            valor-=contas[i];
+            resp++;
+        }
+    }
+
+    cout << resp << endl;
+    return 0;
+}
