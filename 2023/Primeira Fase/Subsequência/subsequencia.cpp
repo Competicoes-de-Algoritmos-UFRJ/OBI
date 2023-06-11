645e04b68beb63823d4db1bd07dcad7edb3420c6
Tiago Silva <tiago.pds.silva@gmail.com>
2023-06-11 17:46:03 -0300

    Update subsequencia.cpp

--- 2023/Primeira Fase/Subsequência/subsequencia.cpp
+++ 2023/Primeira Fase/Subsequência/subsequencia.cpp
@@ -7,18 +7,18 @@ int a, b, sa[100005], sb[100005];
 int main(){
     cin >> a >> b;
 
-    for(int i=1; i<=a; i++){
+    for(int i=0; i<a; i++){
         cin >> sa[i];
     }
 
-    for(int i=1; i<=b; i++){
+    for(int i=0; i<b; i++){
         cin >> sb[i];
     }
 
-    int pont_a = 1;
-    int pont_b = 1;
+    int pont_a = 0;
+    int pont_b = 0;
 
-    while(pont_a<=a && pont_b<=b){
+    while(pont_a<a && pont_b<b){
         
         if(sa[pont_a] == sb[pont_b]){
             pont_b++;
@@ -27,7 +27,7 @@ int main(){
         pont_a++;
     }
 
-    if(pont_b == b+1){
+    if(pont_b == b){
         cout << 'S' << endl;
     }else{
         cout << 'N' << endl;
